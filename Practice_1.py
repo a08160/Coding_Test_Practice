a2682568579ab3f6c37e0fd30e70d4c9b90d8bda
a08160 <a08160@g.skku.edu>
2025-03-12 20:56:57 +0900

    코테 연습 1일차

--- Practice_1.py
+++ Practice_1.py
@@ -19,18 +19,36 @@ def solution(my_string):
 
 # 다른 풀이
 
-# swapcase() 는 대소문자를를 변형하는 함수
+# swapcase() 는 대소문자를 변형하는 함수
 def solution(my_string):
     return my_string.swapcase() 
 
 # lower 과 upper 을 활용해서 대소문자 변형
 def solution(my_string):
     answer = ''
-    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lower = 'abcdefghijklmnopqrstuvwxyz'
-    for c in my_string:
-        if c in upper:
-            answer+=c.lower()
-        elif c in lower:
-            answer+=c.upper()
-    return answer #
+
+    for i in my_string:
+        if i.isupper():
+            answer+=i.lower()
+        else:
+            answer+=i.upper()
+    return answer
+
+'''
+문제2. 인덱스 바꾸기기
+문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+'''
+
+'''
+틀린 풀이
+def solution(my_string, num1, num2):
+    my_string[num1],my_string[num2] = my_string[num2], my_string[num1]
+    return my_string
+
+이유. 문자열(str)은 불변(immutable) 객체이기 때문에 직접 인덱스를 수정할 수 없음음
+'''
+
+def solution(my_string, num1, num2):
+    lst = list(my_string)
+    lst[num1],lst[num2] = lst[num2], lst[num1]
+    return "".join(lst)
