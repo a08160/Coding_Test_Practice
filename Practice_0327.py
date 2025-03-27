92165b3c7d06517289192bced3cdb0048ed035db
a08160 <a08160@g.skku.edu>
2025-03-27 17:54:01 +0900

    3월27일 코테 연습

--- Practice_0327.py
+++ Practice_0327.py
@@ -131,3 +131,44 @@ def solution(id_pw, db):
     if db_pw := dict(db).get(id_pw[0]):
         return "login" if db_pw == id_pw[1] else "wrong pw"
     return "fail"
+
+'''
+문제9. 등수 매기기
+영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+'''
+
+def solution(score):
+    lst = sorted([sum(sco) for sco in score], reverse = True)
+    lst_index = [lst.index(sum(sco))+1 for sco in score]
+
+    return lst_index
+
+print(solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
+
+'''
+문제10. 치킨 쿠폰
+프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+'''
+
+def solution(chicken):
+    answer = 0
+    while True:
+        service = chicken//10
+        answer += service
+        rest = chicken%10
+
+        if service == 0:
+            break
+
+        chicken = service + rest
+
+    return answer
+
+print(solution(1081))
+
+# 다른 사람 풀이.
+
+def solution(chicken):
+    answer = (max(chicken,1)-1)//9
+    return answer
+
