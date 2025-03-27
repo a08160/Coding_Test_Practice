706d2351bda41336c1a4fb0d51bf33a5174a36d8
a08160 <a08160@g.skku.edu>
2025-03-27 14:35:11 +0900

    3월27일 코테 연습

--- Practice_0327.py
+++ Practice_0327.py
@@ -108,3 +108,26 @@ def solution(dots):
         return int(new_lst[1]**2)
     
     return int(new_lst[1]*new_lst[2])
+
+'''
+문제8. 로그인 성공?
+머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+
+아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+'''
+
+def solution(id_pw, db):
+    if id_pw in db:
+        return "login"
+    for id, pw in db:
+        if id_pw[0] == id:
+            return "wrong pw"
+    return "fail"
+
+# 다른 사람 풀이. dict.get(key, default=None): 찾고자하는 키의 value를 반환 없다면 default를 반환
+
+def solution(id_pw, db):
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
