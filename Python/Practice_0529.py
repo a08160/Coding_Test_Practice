d9333da22a543f90c4c5eedbd5a5b16d9b01b544
a08160 <a08160@g.skku.edu>
2025-05-29 17:02:10 +0900

    5월29일 코테 연습

--- Python/Practice_0529.py
+++ Python/Practice_0529.py
@@ -84,3 +84,60 @@ def solution(today, terms, privacies):
             answer.append(i + 1)
 
     return answer
+
+# 문제4. 가장 많이 받은 선물
+
+def solution(friends, gifts):
+    l = len(friends)
+    name_index = {name: idx for idx, name in enumerate(friends)}
+    total_table = [[0] * l for _ in range(l)]
+    quotient_table = [[0] * 3 for _ in range(l)]  # [준 선물, 받은 선물, 차이]
+
+    for a, b in [gift.split() for gift in gifts]:
+        a_idx = name_index[a]
+        b_idx = name_index[b]
+
+        total_table[a_idx][b_idx] += 1
+        quotient_table[a_idx][0] += 1  # 준 선물
+        quotient_table[b_idx][1] += 1  # 받은 선물
+
+    for i in range(l):
+        quotient_table[i][2] = quotient_table[i][0] - quotient_table[i][1]
+
+    result = [0] * l
+
+    for i in range(l):
+        for j in range(i+1, l):
+            a = i
+            b = j
+            if total_table[a][b] > total_table[b][a]:
+                result[a] += 1
+            elif total_table[a][b] < total_table[b][a]:
+                result[b] += 1
+            else:
+                if quotient_table[a][2] > quotient_table[b][2]:
+                    result[a] += 1
+                elif quotient_table[a][2] < quotient_table[b][2]:
+                    result[b] += 1
+                # else: 같으면 선물 없음
+
+    return max(result)
+
+# 문제5. 택배 상자 꺼내기
+
+def solution(n, w, num):
+    num -= 1
+    nums = list(range(0, n))
+    
+    lst = [nums[i:i + w] for i in range(0, n, w)]
+    idx = lst[num//w].index(num)
+    
+    return len(list(filter(lambda x: len(x) > idx, lst)))-num//w
+
+'''
+def solution(n, w, num):
+    m1 = num%(w*2)
+    m2 = ((w*2+1) - m1)%(w*2)
+    # num 이상 n 이하의 수들 중 2*w로 나눈 나머지가 m1,m2인 것들의 수를 세면 된다.
+    return len(range(num,n+1,w*2)) + len(range(num + (m2-m1)%(w*2), n+1, w*2))
+'''
